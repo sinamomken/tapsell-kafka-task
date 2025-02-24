29096c76b9b14c81d84f2225cb1b76d3e926ad7c
Sina Momken <sina.momken@gmail.com>
2025-02-24 17:38:00 +0330

    Created 2 poms for modules consumer and producer

--- consumer/src/main/kotlin/com/example/ConsumerApplication.kt
+++ consumer/src/main/kotlin/com/example/ConsumerApplication.kt
@@ -4,12 +4,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-open class Application
+open class ConsumerApplication
 
 fun main(args: Array<String>) {
     val name = "Kotlin"
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
     println("Hello, " + name + "!")
-    runApplication<Application>(*args);
+    runApplication<ConsumerApplication>(*args);
 }
