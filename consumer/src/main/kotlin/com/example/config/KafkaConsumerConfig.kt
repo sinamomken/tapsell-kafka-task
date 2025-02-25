3ca9b1ff6e982fc054a7f2aedf82e7345dc6f058
Sina Momken <sina.momken@gmail.com>
2025-02-25 17:13:58 +0330

    Add multiTypeConsumerFactory() and multiTypeKafkaListenerContainerFactory()

--- consumer/src/main/kotlin/com/example/config/KafkaConsumerConfig.kt
+++ consumer/src/main/kotlin/com/example/config/KafkaConsumerConfig.kt
@@ -2,13 +2,19 @@ package com.example.config
 
 import com.example.entity.kafka.ClickEvent
 import com.example.entity.kafka.ImpressionEvent
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.serialization.StringDeserializer
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.core.ConsumerFactory
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory
 import org.springframework.kafka.support.converter.RecordMessageConverter
 import org.springframework.kafka.support.converter.StringJsonMessageConverter
 import org.springframework.kafka.support.mapping.DefaultJackson2JavaTypeMapper
 import org.springframework.kafka.support.mapping.Jackson2JavaTypeMapper
+import org.springframework.kafka.support.serializer.JsonDeserializer
 
 
 @Configuration
@@ -30,5 +36,20 @@ open class KafkaConsumerConfig {
         return converter
     }
 
+    @Bean
+    open fun multiTypeConsumerFactory(): ConsumerFactory<String, Any> {
+        val props = HashMap<String, Any>()
+        props[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = bootstrapAddress
+        props[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
+        props[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = JsonDeserializer::class.java
+        return DefaultKafkaConsumerFactory(props)
+    }
 
+    @Bean
+    open fun multiTypeKafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, Any> {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, Any>()
+        factory.consumerFactory = multiTypeConsumerFactory()
+        factory.setRecordMessageConverter(multiTypeConverter())
+        return factory
+    }
 }
---
+++ consumer/src/main/kotlin/com/example/schedule/ConsumerScheduler.kt
@@ -0,0 +1,5 @@
+package com.example.schedule
+
+
+open class ConsumerScheduler {
+}
